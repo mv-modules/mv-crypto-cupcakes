4abcf18f22251eccc393d834ea3c252f3fca797a
Jordan <jordan.mcnamara@gmail.com>
2023-01-31 20:56:14 +0000

    mostly done?

---
+++ .history/index_20230131164441.js
@@ -0,0 +1,53 @@
+require("dotenv").config(".env");
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const { PORT = 3000 } = process.env;
+
+const { User, Cupcake } = require("./db");
+// TODO - require express-openid-connect and destructure auth from it
+const { auth } = require("express-openid-connect");
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: "a long, randomly-generated string stored in env",
+  baseURL: "http://localhost:3000",
+  clientID: "vsaLtIyJOX1UFZbWIPqNu4KPDXO9gEw6",
+  issuerBaseURL: "https://dev-mj4o06irczfkkoi1.us.auth0.com",
+};
+
+// middleware
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/* *********** YOUR CODE HERE *********** */
+// follow the module instructions: destructure config environment variables from process.env
+// follow the docs:
+// define the config object
+// attach Auth0 OIDC auth router
+// create a GET / route handler that sends back Logged in or Logged out
+
+app.get("/cupcakes", async (req, res, next) => {
+  try {
+    const cupcakes = await Cupcake.findAll();
+    res.send(cupcakes);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+// error handling middleware
+app.use((error, req, res, next) => {
+  console.error("SERVER ERROR: ", error);
+  if (res.statusCode < 400) res.status(500);
+  res.send({ error: error.message, name: error.name, message: error.message });
+});
+
+app.listen(PORT, () => {
+  console.log(`Cupcakes are ready at http://localhost:${PORT}`);
+});
---
+++ .history/index_20230131171955.js
@@ -0,0 +1,63 @@
+require("dotenv").config(".env");
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const { PORT = 3000 } = process.env;
+
+const { User, Cupcake } = require("./db");
+// TODO - require express-openid-connect and destructure auth from it
+const { auth, requiresAuth } = require("express-openid-connect");
+const { SECRET, BASE_URL, CLIENT_ID, ISSUER_BASE_URL } = process.env;
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: SECRET,
+  baseURL: BASE_URL,
+  clientID: CLIENT_ID,
+  issuerBaseURL: ISSUER_BASE_URL,
+};
+
+// middleware
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(auth(config));
+app.use(async (req, res, next) => {
+  if (req.oidc.user) {
+    const { nickname, name, email } = req.oidc.user;
+    await User.findOrCreate({
+      username: nickname,
+      name: name,
+      email: email,
+    });
+  }
+  next();
+});
+
+app.get("/profile", (req, res, next) => {
+  res.send(req.oidc.isAuthenticated());
+});
+
+app.get("/cupcakes", async (req, res, next) => {
+  try {
+    const cupcakes = await Cupcake.findAll();
+    res.send(cupcakes);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+// error handling middleware
+app.use((error, req, res, next) => {
+  console.error("SERVER ERROR: ", error);
+  if (res.statusCode < 400) res.status(500);
+  res.send({ error: error.message, name: error.name, message: error.message });
+});
+
+app.listen(PORT, () => {
+  console.log(`Cupcakes are ready at http://localhost:${PORT}`);
+});
